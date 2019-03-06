72a76e674898a2e24609ea5af5a5731e87352e7f
unknown <vovapilimon@gmail.com>
2019-03-06 13:01:03 +0300

    Updates

---
+++ precache-manifest.3242f0f08ac8b4fd77716946e46db846.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42b3f677a305eb72996f",
+    "url": "/courses/static/css/main.818134e6.chunk.css"
+  },
+  {
+    "revision": "42b3f677a305eb72996f",
+    "url": "/courses/static/js/main.42b3f677.chunk.js"
+  },
+  {
+    "revision": "871407345623f6a96f21",
+    "url": "/courses/static/js/runtime~main.87140734.js"
+  },
+  {
+    "revision": "0b847a6b7c95e52657d0",
+    "url": "/courses/static/css/2.618c8b02.chunk.css"
+  },
+  {
+    "revision": "0b847a6b7c95e52657d0",
+    "url": "/courses/static/js/2.0b847a6b.chunk.js"
+  },
+  {
+    "revision": "033e14999faf75bc5289bd9899f67af3",
+    "url": "/courses/index.html"
+  }
+];
